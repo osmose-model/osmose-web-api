45393d50e6e7e7cbe8254074c8420054e308088c
Jorrit Poelen <jhpoelen@gmail.com>
2015-12-10 11:04:51 -0800

    reformatting

--- src/main/java/com/github/jhpoelen/fbob/Config.java
+++ src/main/java/com/github/jhpoelen/fbob/Config.java
@@ -3,9 +3,7 @@ package com.github.jhpoelen.fbob;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.InputStream;
 
 @Path("osmose_config.zip")
 public class Config {
@@ -13,6 +11,9 @@ public class Config {
     @GET
     @Produces("application/zip")
     public Response configArchive() {
-        return Response.ok(getClass().getResourceAsStream("osmose_config.zip")).header("Content-Disposition", "attachment; filename=osmose_config.zip").build();
+        return Response
+                .ok(getClass().getResourceAsStream("osmose_config.zip"))
+                .header("Content-Disposition", "attachment; filename=osmose_config.zip")
+                .build();
     }
 }
--- src/main/java/com/github/jhpoelen/fbob/Ping.java
+++ src/main/java/com/github/jhpoelen/fbob/Ping.java
@@ -7,13 +7,6 @@ import javax.ws.rs.core.MediaType;
 
 @Path("ping")
 public class Ping {
-
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
