1ec21e2e5cfb53452bdca481d42e17fab19dff49
Jorrit Poelen <jhpoelen@gmail.com>
2016-09-06 19:00:50 -0700

    make cors headers more specific

--- src/main/java/com/github/jhpoelen/fbob/CORSResponseFilter.java
+++ src/main/java/com/github/jhpoelen/fbob/CORSResponseFilter.java
@@ -15,8 +15,8 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
         headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        headers.add("Access-Control-Allow-Methods", "POST");
     }
 
 }
