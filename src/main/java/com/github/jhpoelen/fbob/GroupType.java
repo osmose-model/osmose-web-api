779271dce982155ab309ead200855e841302f376
QQ-Sortiz <mielortiz@gmail.com>
2018-03-09 10:56:19 +0800

    Updated background value
    Changed background to biotic_resource

--- src/main/java/com/github/jhpoelen/fbob/GroupType.java
+++ src/main/java/com/github/jhpoelen/fbob/GroupType.java
@@ -5,6 +5,6 @@ import javax.xml.bind.annotation.XmlEnumValue;
 
 @XmlEnum(String.class)
 public enum GroupType {
-    @XmlEnumValue("background") BACKGROUND,
+    @XmlEnumValue("biotic_resource") BACKGROUND,
     @XmlEnumValue("focal_functional_group") FOCAL
 }
