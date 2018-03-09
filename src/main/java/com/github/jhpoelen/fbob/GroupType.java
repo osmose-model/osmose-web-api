d136702e57b759fc21251249788e751bb32be7fe
QQ-Sortiz <mielortiz@gmail.com>
2018-03-09 13:15:22 +0800

    Reverted group type value
    Reverted group type value to FOCAL and BACKGROUND

--- src/main/java/com/github/jhpoelen/fbob/GroupType.java
+++ src/main/java/com/github/jhpoelen/fbob/GroupType.java
@@ -5,6 +5,6 @@ import javax.xml.bind.annotation.XmlEnumValue;
 
 @XmlEnum(String.class)
 public enum GroupType {
-    @XmlEnumValue("biotic_resource") biotic_resource,
-    @XmlEnumValue("focal_functional_group") focal_functional_group
+    @XmlEnumValue("biotic_resource") BACKGROUND,
+    @XmlEnumValue("focal_functional_group") FOCAL
 }
