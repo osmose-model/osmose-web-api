d18663c63f1588efb713843fbc09a21431f7a68c
QQ-Sortiz <mielortiz@gmail.com>
2018-03-23 15:02:38 +0800

    Reverted value of group type
    Reverted value of group type to working group

--- src/main/java/com/github/jhpoelen/fbob/GroupType.java
+++ src/main/java/com/github/jhpoelen/fbob/GroupType.java
@@ -5,8 +5,6 @@ import javax.xml.bind.annotation.XmlEnumValue;
 
 @XmlEnum(String.class)
 public enum GroupType {
-    @XmlEnumValue("biotic_resource") 
-    BACKGROUND("biotic_resource"),
-    @XmlEnumValue("focal_functional_group") 
-    FOCAL("focal_functional_group")
+    @XmlEnumValue("biotic_resource")BACKGROUND,
+    @XmlEnumValue("focal_functional_group") FOCAL
 }
